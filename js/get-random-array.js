e4c4178cbf02dfd641658c3b1d46ffab9854c3f4
Вячеслав Ярлыканов <yarlyk@mail.ru>
2024-07-22 16:53:31 +0400

    параметр Array с большой буквы исправил на маленькую

--- js/get-random-array.js
+++ js/get-random-array.js
@@ -1,15 +1,15 @@
 import { getRandomInteger } from './get-random-array-element.js';
 
-function getRandomArray(Array) {
+function getRandomArray(array) {
   const previousValues = [];
-  const lengthArray = getRandomInteger(1, Array.length);
-  let currentValue = Array[getRandomInteger(0, Array.length - 1)];
+  const lengthArray = getRandomInteger(1, array.length);
+  let currentValue = array[getRandomInteger(0, array.length - 1)];
   for (let index = 0; index < lengthArray; index++) {
     while (
       previousValues.includes(currentValue) ||
       previousValues.length >= lengthArray
     ) {
-      currentValue = Array[getRandomInteger(0, Array.length - 1)];
+      currentValue = array[getRandomInteger(0, array.length - 1)];
     }
     previousValues.push(currentValue);
   }
